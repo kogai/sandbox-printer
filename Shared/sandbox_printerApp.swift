06094554d0409045d7757ccb1f3edb1a7bd23b89
shinichi kogai <kogai0121@gmail.com>
2021-01-19 09:59:45 +0900

    Initial Commit

---
+++ Shared/sandbox_printerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandbox_printerApp.swift
+//  Shared
+//
+//  Created by Shi'n'ichi Kogai on 2021/01/19.
+//
+
+import SwiftUI
+
+@main
+struct sandbox_printerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
